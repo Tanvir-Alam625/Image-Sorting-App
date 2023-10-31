d30c453af4455c036910d0f1821320d18e930122
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-11-01 01:55:20 +0600

    chore: select able functionality added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,29 @@
 import { BsCheckSquareFill } from 'react-icons/bs';
 
 type Props = {
-  selected: number[];
+  selected: number;
 };
 
 const Header = ({ selected }: Props) => {
   return (
     <div className="pt-3 px-3 pb-1 border-b border-slate-300">
-      {selected.length === 0 ? (
+      {selected === 0 ? (
         <h1 className="text-2xl font-medium text-left text-slate-800">
           Gallery
         </h1>
       ) : null}
-      {selected.length ? (
+      {selected ? (
         <div className="flex justify-between items-center">
           <div className="flex items-center gap-2">
             <span className="text-blue-600 inline-block text-xl">
               <BsCheckSquareFill className="inline" />
             </span>
             <h2 className="text-xl font-medium text-left text-slate-800">
-              {selected.length > 2
-                ? `${selected.length} Files`
-                : `${selected.length} File`}{' '}
-              selected
+              {selected > 2 ? `${selected} Files` : `${selected} File`} selected
             </h2>
           </div>
           <button className="reset-btn text-xl font-medium text-left text-red-600">
-            Delete {selected.length > 2 ? 'files' : 'file'}
+            Delete {selected > 2 ? 'files' : 'file'}
           </button>
         </div>
       ) : null}
